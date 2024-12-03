25e7f47295e998e08ba2c9310fb2d2b866760d9e
stae01 <166432863+stae01@users.noreply.github.com>
2024-12-03 16:21:25 -0700

    Update Encriptacion.java

--- NegocioSoundByte/src/main/java/util/Encriptacion.java
+++ NegocioSoundByte/src/main/java/util/Encriptacion.java
@@ -12,20 +12,42 @@ import at.favre.lib.crypto.bcrypt.BCrypt;
  */
 public class Encriptacion {
 
+    /**
+     * Declaracón de variables
+     */
     public static final int COST = 12;
 
+    /**
+     * Método constructor
+     */
     private Encriptacion() {
         throw new IllegalStateException("Utility class");
     }
 
+    /**
+     * Mpetodo para encriptar password
+     * @param password String a encriptar
+     * @return regreesa la contraseña encriptada
+     */
     public static String encriptarPassword(String password) {
         return BCrypt.withDefaults().hashToString(COST, password.toCharArray());
     }
 
+    /**
+     * Método para encriptar la contraseña
+     * @param password contraseña a encriptar
+     * @return regresa la contraseña encriptada
+     */
     public static String encriptarPassword(char[] password) {
         return BCrypt.withDefaults().hashToString(COST, password);
     }
 
+    /**
+     * Método para verificar la contraseña
+     * @param password contraseña
+     * @param hashedPassword contraseña con hash
+     * @return regresa el resultado de la verificación
+     */
     public static boolean verificarPasswordConHash(String password, String hashedPassword) {
         BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), hashedPassword);
         return result.verified;
